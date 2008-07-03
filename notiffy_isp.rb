545534947bf2d879ae199f1f9d314e7fcf18710e
Nazar Aziz <nazar@panthersoftware.com>
2008-07-03 15:08:35 +0100

    fixing bugs

--- notiffy_isp.rb
+++ notiffy_isp.rb
@@ -1,4 +1,4 @@
-!# /usr/bin/ruby
+#! /usr/bin/ruby
 
 require 'net/smtp'
 
@@ -9,9 +9,9 @@ SMTP_PORT   = 25
 EMAIL_FROM    = 'admin@panthersoftware.com'
 EMAIL_SUBJECT = 'Security Alert - Your Server Has Been Hacked!'
 #guess apps... override if required
-GREP_BIN  = `which grep`
-CAT_BIN   = `which cat`
-WHOIS_BIN = `which whois`
+GREP_BIN  = `which grep`.strip
+CAT_BIN   = `which cat`.strip
+WHOIS_BIN = `which whois`.strip
 
 #LOG_FILE = SSHD's log file
 LOG_FILE = '/var/log/pwdfail/current'
@@ -20,9 +20,10 @@ LOG_FILE = '/var/log/pwdfail/current'
 TIME_LOCALE = 'GMT'
 
 #check that we have all our BINs
-raise 'Could not find grep on your system. Manually configure GREP_BIN' if GREP_BIN.blank?
-raise 'Could not find cat on your system. Manually configure CAT_BIN' if CAT_BIN.blank?
-raise 'Could not find whois on your system. Manually configure WHOIS_BIN' if WHOIS_BIN.blank?
+raise 'Could not find grep on your system. Manually configure GREP_BIN' if GREP_BIN == ''
+raise 'Could not find cat on your system. Manually configure CAT_BIN' if CAT_BIN == ''
+raise 'Could not find whois on your system. Manually configure WHOIS_BIN' if WHOIS_BIN == ''
+
 
 ################# UTILS ########################
 def time2str( tm )
@@ -32,37 +33,36 @@ def time2str( tm )
   offset = tm.to_i - Time.local(*gmt.to_a[0,6].reverse).to_i
 
   sprintf '%s, %s %s %d %02d:%02d:%02d %+.2d%.2d',
-          WDAY[tm.wday], tm.mday, MONTH[tm.month],
+          tm.strftime('%a'), tm.mday, tm.strftime('%B'),
           tm.year, tm.hour, tm.min, tm.sec,
           *(offset / 60).divmod(60)
 end
 
-def get_email_message(to_address)
+def get_email_message(to_address, offender, evidence)
   
 email_message = <<EOF
 From: #{EMAIL_FROM}
-Reply
 To: #{to_address}
 Subject: #{EMAIL_SUBJECT}
-Date: #{time2str(Time.now}
+Date: #{time2str(Time.now)}
 
-To whome it may concern.
+To whom it may concern.
 
-We have detected a hack attempt which has originated from your network from ip: #{host}
+We have detected a hack attempt which has originated from your network from ip: #{offender}
 
-This suggests that the above server has been comprimised and is a participant in a bot net.
+This suggests that the above server has been compromised and is a participant in a botnet.
 
 This means that your server has been hack and now, in turn, is attempting to hack other servers on the Internet.
 
-This IP address has now been blacklisted to prtected our service from further brute force attacks.
-Furthermore, this IP address has been uploaded to a cetralised database. This means
-that this IP address will also shortly be blacklisted by any member who queries this central database.
+This IP address has now been blacklisted to protect our service from further brute force attacks. Furthermore, this IP address has been uploaded to a centralised database. This means that this IP address will also shortly be blacklisted by any member who queries this central database.
 
 An excerpt from our logfiles. All times shown are in #{TIME_LOCALE}:
 
 #{evidence}
 
 Regards.
+Panther Software Admin
+
 EOF
   
 end
@@ -78,20 +78,20 @@ end
 
 #extract all email contacts from given host
 contacts = []
-result = eval("`#{WHOIS_BIN #{host}} | #{GREP_BIN} e-mail`")
-result.each_line do |line|
+result = eval("`#{WHOIS_BIN} #{host} | #{GREP_BIN} e-mail`")
+result.each_line do |line| 
   email = line[/([-a-z0-9]+[\w\.\-\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i]
-  contacts << email unless email.blank?
+  contacts << email unless email == ''
 end
 
 #extract evidence from ssh log file
-evidence = eval("`#{CAT_BIN}` #{LOG_FILE} | #{GREP_BIN} #{host}")
-
+evidence = eval("`#{CAT_BIN} #{LOG_FILE} | #{GREP_BIN} #{host}`")
 
 Net::SMTP.start(SMTP_SERVER, SMTP_PORT) do |smtp|
+  
   #send email to each returned address
   contacts.each do |email|
-    puts get_email_message(email)
+    puts get_email_message(email, host, evidence)
     # smtp.send_message get_email_message(email),
         #                   EMAIL_FROM,
         #                   email
