fa47bc427a1d57d99231266573a2578d98bb788e
Nazar Aziz <nazar@panthersoftware.com>
2008-07-09 12:30:51 +0100

    # Update to lookup contact info on top level domain names as ip addresses don't always return contact info # Raise exception to notify that no contact emails were found # Add timestamp to log file

--- notify_isp.rb
+++ notify_isp.rb
@@ -18,18 +18,20 @@ EMAIL_SUBJECT = 'Security Alert - Your Server May Have Been Hacked!'
 GREP_BIN  = `which grep`.strip
 CAT_BIN   = `which cat`.strip
 WHOIS_BIN = `which whois`.strip
+HOST_BIN  = `which host`.strip
 
 #LOG_FILE = SSHD's log file
 LOG_FILE = '/var/log/sshd/*'
 
 #misc
-TIME_LOCALE = 'GMT'
+TIME_LOCALE = 'GMT+1'
 EMAIL_LOG_FILE    = '/var/log/notify_isp.log'
 
 #check that we have all our BINs
 raise 'Could not find grep on your system. Manually configure GREP_BIN' if GREP_BIN == ''
 raise 'Could not find cat on your system. Manually configure CAT_BIN' if CAT_BIN == ''
 raise 'Could not find whois on your system. Manually configure WHOIS_BIN' if WHOIS_BIN == ''
+raise 'Could not find host on your system. Manually configure HOST_BIN' if HOST_BIN == ''
 
 
 ################# UTILS ########################
@@ -72,6 +74,20 @@ EOF
 
 end
 
+def get_contacts_for_host(lookup_host)
+  result = []
+  lookup = eval("`#{WHOIS_BIN} #{lookup_host} | #{GREP_BIN} @`") #return any line that contains an @ symbol
+  lookup.each_line do |line|
+    email = line[/([-a-z0-9]+[\w\.\-\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})/i]
+    result << email unless email == nil
+  end
+  #if contacts includes an abuse@ address then only send it to those.
+  tmp = result.select { |email| email[/abuse@/] }
+  result = tmp if tmp.length > 0
+  result.uniq! if result.length > 1
+  return result.uniq
+end
+
 ################# MAIN ##########################
 
 #extract ip/domain from passed parameter
@@ -82,28 +98,39 @@ else
 end
 
 #extract all email contacts for given host
-contacts = []
-lookup = eval("`#{WHOIS_BIN} #{host} | #{GREP_BIN} e-mail`")
-lookup.each_line do |line|
-  email = line[/([-a-z0-9]+[\w\.\-\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})/i]
-  contacts << email unless email == nil
+contacts = get_contacts_for_host(host)
+
+#lookup top level domain name and extract domain contact info
+#if given ip then lookup to hostname
+if host[/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/]
+  host_domain = eval("`#{HOST_BIN} #{host}`").strip
+  host_domain = host_domain[/.+\.(\w+\.\w+)/,1]
+else
+  host_domain = host[/.+\.(\w+\.\w+)/,1]
 end
 
-#if contacts includes an abuse@ address then only send it to those.
-tmp = contacts.select { |email| email[/abuse@/] }
-contacts = tmp.unique if tmp.length > 0
+if host_domain
+  domain_contacts = get_contacts_for_host(host_domain)
+  contacts << domain_contacts if domain_contacts.length > 0
+end
 
-#extract evidence from ssh log file
+#filter out duplicates one last time
+contacts.uniq! if contacts.length > 1
+
+raise "No email addresses were returned" unless contacts.length > 0
+
+#extract evidence from ssh log file using the reported host as a filter
 evidence = eval("`#{CAT_BIN} #{LOG_FILE} | #{GREP_BIN} #{host}`")
 
 Net::SMTP.start(SMTP_SERVER, SMTP_PORT) do |smtp|
+  
   begin
     #send email to each returned address
-    contacts.each do |email|
+    contacts.faltten.each do |email|
       smtp.send_message get_email_message(email, host, evidence), EMAIL_FROM, email
       #log ip address and email 
       my_file = File.new(EMAIL_LOG_FILE, 'a+')
-      my_file.puts "Report generated for #{host} and sent to #{email}"
+      my_file.puts "Report generated for #{host} and sent to #{email} on #{Time.now.to_s}"
     end
   ensure
     smtp.finish
